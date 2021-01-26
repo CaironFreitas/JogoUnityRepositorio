9f91bb71d7cf4d0af95d244d8a58cc0af896112d
Cairon Sérgio de Freitas <cairon.sergio@gmail.com>
2021-01-26 20:54:35 -0300

    Event window
    - Task "First enemy" Done

---
+++ Assets/Scenes/Scene Gameplay/Scripts/scpCreateEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scpCreateEnemy : MonoBehaviour
+{
+    public GameObject pobjDefaultEnemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int liEnemyCount = GameObject.FindGameObjectsWithTag("EnemyDefault").Length;
+
+        if (liEnemyCount < 2)
+        {
+            if (staticScpMapProperties.psbMapaCriado)
+            {
+                int liHeight = Random.Range(0, staticScpMapProperties.psiMapHeight);
+                int liWeight = Random.Range(0, staticScpMapProperties.psiMapWeight);
+
+                string lsTileName = liHeight + "," + liWeight;
+
+                GameObject lobjTileRandom = GameObject.Find(lsTileName);
+
+                GameObject CloneDefaultEnemy = Instantiate(pobjDefaultEnemy);
+
+                CloneDefaultEnemy.transform.position = lobjTileRandom.transform.position;
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Scene Gameplay/Scripts/staticScpMapProperties.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class staticScpMapProperties : MonoBehaviour
+{
+    public static int psiMapHeight = 8;
+    public static int psiMapWeight = 8;
+
+    public static bool psbMapaCriado;
+}
