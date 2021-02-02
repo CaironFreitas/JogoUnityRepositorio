f196a22cd49614cc1093ddee5ee560d7366de272
Cairon Sérgio de Freitas <cairon.sergio@gmail.com>
2021-02-02 00:34:55 -0300

    Event Window
    - Player actions

--- Assets/Windows/WindowEvent/Scripts/scpClickActionButton.cs
+++ Assets/Windows/WindowEvent/Scripts/scpClickActionButton.cs
@@ -9,10 +9,15 @@ public class scpClickActionButton : MonoBehaviour
     public scpEnemyInfo pscpEnemyInfo;
 
     private ScpEscreveAcoes pscpEscreveAcoes;
+    private scpPlayerActions pscpPlayerActions;
 
     private void Start()
     {
-        pscpEscreveAcoes = GameObject.Find("ContentHistoric").GetComponent<ScpEscreveAcoes>();
+        pscpEscreveAcoes  = GameObject.Find("ContentHistoric").GetComponent<ScpEscreveAcoes>();
+        pscpPlayerActions = this.GetComponent<scpPlayerActions>();
+
+        pscpPlayerActions.pscpEscreveAcoes = pscpEscreveAcoes;
+        pscpPlayerActions.pscpEnemyInfo = pscpEnemyInfo;
     }
 
     public void metClickButton()
@@ -22,13 +27,13 @@ public class scpClickActionButton : MonoBehaviour
             switch (plActionCode)
             {
                 case 0: //-- Melee Combat
-                    metMeleeCombat();
+                    pscpPlayerActions.metMeleeCombat();
                     break;
                 case 1: //-- Flee
-                    metFlee();
+                    pscpPlayerActions.metFlee();
                     break;
                 case 2: //-- Hide
-                    metHide();
+                    pscpPlayerActions.metHide();
                     break;
             }   
         }
@@ -39,71 +44,5 @@ public class scpClickActionButton : MonoBehaviour
             {
             }
         }*/
-    }
-
-    private void metMeleeCombat()
-    {
-        long llDefaultHitChance = 70; 
-        long llSkillDiff = 3; 
-        long llEnemyMelee = pscpEnemyInfo.plMelee;
-        long llPlayerMelee = StaticPersonagem.plMelee;
-
-        long llPlayerHitChance = ((llPlayerMelee - llEnemyMelee) * llSkillDiff) + llDefaultHitChance;
-        Debug.Log(llPlayerHitChance + "% hit chance");
-
-        long llChance = Random.Range(0, 100);
-
-        if (llChance <= llPlayerHitChance)
-        {
-            pscpEscreveAcoes.metDefaultText("You", "Acerta o estranho");
-        }
-        else
-        {
-            pscpEscreveAcoes.metDefaultText("You", "Erra o ataque");
-        }
-    }
-
-    private void metFlee()
-    {
-        long llDefaultFleeChance = 50;
-        long llSkillDiff = 5;
-        long llEnemyAthletics = pscpEnemyInfo.plAthletics;
-        long llPlayerAthletics = StaticPersonagem.plAthletics;
-
-        long llPlayerFleeChance = ((llPlayerAthletics - llEnemyAthletics) * llSkillDiff) + llDefaultFleeChance;
-        Debug.Log(llPlayerFleeChance + "% flee chance");
-
-        long llChance = Random.Range(0, 100);
-
-        if (llChance <= llPlayerFleeChance)
-        {
-            pscpEscreveAcoes.metDefaultText("You", "Conseguiu fugir");
-        }
-        else
-        {
-            pscpEscreveAcoes.metDefaultText("You", "Não conseguiu fugir");
-        }
-    }
-
-    private void metHide()
-    {
-        long llDefaultHideChance = 10;
-        long llSkillDiff = 5;
-        long llEnemyHide = pscpEnemyInfo.plSneak;
-        long llPlayerHide = StaticPersonagem.plSneak;
-
-        long llPlayerHideChance = ((llPlayerHide - llEnemyHide) * llSkillDiff) + llDefaultHideChance;
-        Debug.Log(llPlayerHideChance + "% hide chance");
-
-        long llChance = Random.Range(0, 100);
-
-        if (llChance <= llPlayerHideChance)
-        {
-            pscpEscreveAcoes.metDefaultText("You", "Se escondeu");
-        }
-        else
-        {
-            pscpEscreveAcoes.metDefaultText("You", "Foi visto tentando se esconder");
-        }
     }
 }
---
+++ Assets/Windows/WindowEvent/Scripts/scpPlayerActions.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scpPlayerActions : MonoBehaviour
+{
+    public scpEnemyInfo pscpEnemyInfo;
+    public ScpEscreveAcoes pscpEscreveAcoes;
+
+    public void metMeleeCombat()
+    {
+        long llDefaultHitChance = 70;
+        long llSkillDiff = 3;
+        long llEnemyMelee = pscpEnemyInfo.plMelee;
+        long llPlayerMelee = StaticPersonagem.plMelee;
+
+        long llPlayerHitChance = ((llPlayerMelee - llEnemyMelee) * llSkillDiff) + llDefaultHitChance;
+        Debug.Log(llPlayerHitChance + "% hit chance");
+
+        long llChance = Random.Range(0, 100);
+
+        if (llChance <= llPlayerHitChance)
+        {
+            pscpEscreveAcoes.metDefaultText("You", "Acerta o estranho");
+        }
+        else
+        {
+            pscpEscreveAcoes.metDefaultText("You", "Erra o ataque");
+        }
+    }
+
+    public void metFlee()
+    {
+        long llDefaultFleeChance = 50;
+        long llSkillDiff = 5;
+        long llEnemyAthletics = pscpEnemyInfo.plAthletics;
+        long llPlayerAthletics = StaticPersonagem.plAthletics;
+
+        long llPlayerFleeChance = ((llPlayerAthletics - llEnemyAthletics) * llSkillDiff) + llDefaultFleeChance;
+        Debug.Log(llPlayerFleeChance + "% flee chance");
+
+        long llChance = Random.Range(0, 100);
+
+        if (llChance <= llPlayerFleeChance)
+        {
+            pscpEscreveAcoes.metDefaultText("You", "Conseguiu fugir");
+        }
+        else
+        {
+            pscpEscreveAcoes.metDefaultText("You", "Não conseguiu fugir");
+        }
+    }
+
+    public void metHide()
+    {
+        long llDefaultHideChance = 10;
+        long llSkillDiff = 5;
+        long llEnemyHide = pscpEnemyInfo.plSneak;
+        long llPlayerHide = StaticPersonagem.plSneak;
+
+        long llPlayerHideChance = ((llPlayerHide - llEnemyHide) * llSkillDiff) + llDefaultHideChance;
+        Debug.Log(llPlayerHideChance + "% hide chance");
+
+        long llChance = Random.Range(0, 100);
+
+        if (llChance <= llPlayerHideChance)
+        {
+            pscpEscreveAcoes.metDefaultText("You", "Se escondeu");
+        }
+        else
+        {
+            pscpEscreveAcoes.metDefaultText("You", "Foi visto tentando se esconder");
+        }
+    }
+}
